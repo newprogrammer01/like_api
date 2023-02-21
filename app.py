3be5a793f49f5822a08010d27846a795d1c2fafe
ZARIFMAC <uzbzarif@gmail.com>
2023-02-21 09:40:33 +0500

    home endpoint

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 # Import libraries
 from flask import Flask
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True)
+
